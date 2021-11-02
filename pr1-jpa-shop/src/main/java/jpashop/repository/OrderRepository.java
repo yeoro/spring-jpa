a24bf09e354561bb7540794fd63397449e17e8c6
yeoro <dufgh1009@naver.com>
2021-11-02 17:30:17 +0900

    [ADD] 동적쿼리

--- pr1-jpa-shop/src/main/java/jpashop/repository/OrderRepository.java
+++ pr1-jpa-shop/src/main/java/jpashop/repository/OrderRepository.java
@@ -1,10 +1,19 @@
 package jpashop.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +32,70 @@ public class OrderRepository {
 		return em.find(Order.class, id);
 	}
 	
-	public List<Order> findAll() {
-		return null;
+	// 하드한 동적쿼리
+	public List<Order> findAll(OrderSearch orderSearch) {
+		
+		String jpql = "select o from Order o join o.member m";
+		boolean isFirstCondition = true;
+		
+		// 주문 상태 검색
+		if(orderSearch.getOrderStatus() != null) {
+			if(isFirstCondition) {
+				jpql += " where";
+				isFirstCondition = false;
+			} else {
+				jpql += " and";
+			}
+		}
+		jpql += " o.status = :status";
+		
+		// 회원 이름 검색
+		if(StringUtils.hasText(orderSearch.getMemberName())) {
+			if(isFirstCondition) {
+				jpql += " where";
+				isFirstCondition = false;
+			} else {
+				jpql += " and";
+			}
+		}
+		jpql += " m.name like :name";
+		
+		TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+									.setMaxResults(1000);
+		
+		if(orderSearch.getOrderStatus() != null) {
+			query = query.setParameter("status", orderSearch.getOrderStatus());
+		}
+		if(StringUtils.hasText(orderSearch.getMemberName())) {
+			query = query.setParameter("name", orderSearch.getMemberName());
+		}
+		
+		return query.getResultList();
+	}
+	
+	// JPA Criteria : JPA에서 표준으로 제공하는 동적쿼리 빌드
+	public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+		Root<Order> o = cq.from(Order.class);
+		Join<Object, Object> m = o.join("member", JoinType.INNER);
+		
+		List<Predicate> criteria = new ArrayList<>();
+		
+		// 주문 상태 검색
+		if(orderSearch.getOrderStatus() != null) {
+			Predicate status = (Predicate) cb.equal(o.get("status"), orderSearch.getOrderStatus());
+			criteria.add(status);
+		}
+		
+		if(StringUtils.hasText(orderSearch.getMemberName())) {
+			Predicate name = (Predicate) cb.equal(o.get("name"), "%" + orderSearch.getMemberName() + "%");
+			criteria.add(name);
+		}
+		
+		cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+		TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+		
+		return query.getResultList();
 	}
 }
---
+++ pr1-jpa-shop/src/main/java/jpashop/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package jpashop.repository;
+
+import jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+	
+	private String memberName;
+	private OrderStatus orderStatus;
+}
