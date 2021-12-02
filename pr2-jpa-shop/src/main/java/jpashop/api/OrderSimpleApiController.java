a40ede2fcde78f49271fe08500de6e023b2868db
yeoro <dufgh1009@naver.com>
2021-12-02 15:53:17 +0900

    [CREATE] order v2 - DTO

--- pr2-jpa-shop/src/main/java/jpashop/api/OrderSimpleApiController.java
+++ pr2-jpa-shop/src/main/java/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpashop.domain.Address;
 import jpashop.domain.Order;
+import jpashop.domain.OrderStatus;
 import jpashop.repository.OrderRepository;
 import jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -30,5 +36,33 @@ public class OrderSimpleApiController {
 			order.getDelivery().getAddress();
 		}
 		return all;
+	}
+	
+	@GetMapping("/api/v2/simple-orders")
+	public List<SimpleOrderDto> ordersV2() {
+		List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+		List<SimpleOrderDto> result = orders.stream()
+			.map(o -> new SimpleOrderDto(o))
+			.collect(Collectors.toList());
+		
+		return result;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class SimpleOrderDto {
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+		
+		public SimpleOrderDto(Order o) {
+			orderId = o.getId();
+			name = o.getMember().getName(); // Lazy 강제 초기화
+			orderDate = o.getOrderDate();
+			orderStatus = o.getStatus();
+			address = o.getDelivery().getAddress(); // Lazy 강제 초기화
+		}
 	}
 }
