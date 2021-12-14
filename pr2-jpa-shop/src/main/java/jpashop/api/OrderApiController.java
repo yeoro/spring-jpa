be362b85d8303bf2e980cd0ac84492ede14d9d13
yeoro <dufgh1009@naver.com>
2021-12-14 10:20:18 +0900

    [ADD] order V3.1 (xToMany) - Dto + fetch join + paging

--- pr2-jpa-shop/src/main/java/jpashop/api/OrderApiController.java
+++ pr2-jpa-shop/src/main/java/jpashop/api/OrderApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpashop.domain.Address;
@@ -51,6 +52,18 @@ public class OrderApiController {
 		List<OrderDto> collect = orders.stream()
 			.map(o -> new OrderDto(o))
 			.collect(Collectors.toList());
+		
+		return collect;
+	}
+	
+	@GetMapping("/api/v3.1/orders")								
+	public List<OrderDto> ordersV3_page(
+			@RequestParam(value = "offset", defaultValue = "0") int offset,
+			@RequestParam(value = "limit", defaultValue = "100") int limit) {
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+		List<OrderDto> collect = orders.stream()
+				.map(o -> new OrderDto(o))
+				.collect(Collectors.toList());
 		
 		return collect;
 	}
