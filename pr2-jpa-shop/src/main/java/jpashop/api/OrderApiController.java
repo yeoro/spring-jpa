64175314e24a87b9adb5e29382ce1b4b37ed7f48
yeoro <dufgh1009@naver.com>
2021-12-14 11:24:07 +0900

    [ADD] order V4 (xToMany) - search Dto in repository

--- pr2-jpa-shop/src/main/java/jpashop/api/OrderApiController.java
+++ pr2-jpa-shop/src/main/java/jpashop/api/OrderApiController.java
@@ -14,6 +14,8 @@ import jpashop.domain.OrderItem;
 import jpashop.domain.OrderStatus;
 import jpashop.repository.OrderRepository;
 import jpashop.repository.OrderSearch;
+import jpashop.repository.order.query.OrderQueryDto;
+import jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderApiController {
 
 	private final OrderRepository orderRepository;
+	private final OrderQueryRepository orderQueryRepository;
 	
 	@GetMapping("/api/v1/orders")
 	public List<Order> ordersV1() {
@@ -66,6 +69,11 @@ public class OrderApiController {
 				.collect(Collectors.toList());
 		
 		return collect;
+	}
+	
+	@GetMapping("/api/v4/orders")
+	public List<OrderQueryDto> ordersV4() {
+		return orderQueryRepository.findOrderQueryDtos();
 	}
 	
 	@Getter
---
+++ pr2-jpa-shop/src/main/java/jpashop/repository/order/query/OrderQueryDto.java
@@ -0,0 +1,28 @@
+package jpashop.repository.order.query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jpashop.domain.Address;
+import jpashop.domain.OrderStatus;
+import lombok.Data;
+
+@Data
+public class OrderQueryDto {
+
+	private Long orderId;
+	private String name;
+	private LocalDateTime orderDate;
+	private OrderStatus orderStatus;
+	private Address address;
+	private List<OrderItemQueryDto> orderItems;
+	
+	public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus,
+			Address address) {
+		this.orderId = orderId;
+		this.name = name;
+		this.orderDate = orderDate;
+		this.orderStatus = orderStatus;
+		this.address = address;
+	} 
+}
