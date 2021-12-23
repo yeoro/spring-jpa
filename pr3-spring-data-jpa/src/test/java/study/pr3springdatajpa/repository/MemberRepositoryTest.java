43d9db9c8a6367b52ef1c45c0ea545e7831613a2
yeoro <dufgh1009@naver.com>
2021-12-23 15:04:35 +0900

    [ADD] member repository interface crud test

--- pr3-spring-data-jpa/src/test/java/study/pr3springdatajpa/repository/MemberRepositoryTest.java
+++ pr3-spring-data-jpa/src/test/java/study/pr3springdatajpa/repository/MemberRepositoryTest.java
@@ -2,6 +2,8 @@ package study.pr3springdatajpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,8 @@ class MemberRepositoryTest {
 	
 	@Test
 	public void testMember() {
+		System.out.println("memberRepository = " + memberRepository.getClass());
+		
 		Member member = new Member("memberA");
 		Member saveMember = memberRepository.save(member);
 		
@@ -29,6 +33,34 @@ class MemberRepositoryTest {
 		assertThat(findMember.getId()).isEqualTo(member.getId());
 		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 		assertThat(findMember).isEqualTo(member);
+	}
+	
+	@Test
+	public void basicCRUD() {
+		Member member1 = new Member("member1");
+		Member member2 = new Member("member2");
+		memberRepository.save(member1);
+		memberRepository.save(member2);
 		
+		// 단 건 조회 검증
+		Member findMember1 = memberRepository.findById(member1.getId()).get();
+		Member findMember2 = memberRepository.findById(member2.getId()).get();
+		assertThat(findMember1).isEqualTo(member1);
+		assertThat(findMember2).isEqualTo(member2);
+		
+		// 리스트 조회 검증
+		List<Member> all = memberRepository.findAll();
+		assertThat(all.size()).isEqualTo(2);
+		
+		// 카운트 검증
+		Long count = memberRepository.count();
+		assertThat(count).isEqualTo(2);
+		
+		// 삭제 검증
+		memberRepository.delete(findMember1);
+		memberRepository.delete(findMember2);
+		
+		Long deletedCount = memberRepository.count();
+		assertThat(deletedCount).isEqualTo(0);
 	}
 }
