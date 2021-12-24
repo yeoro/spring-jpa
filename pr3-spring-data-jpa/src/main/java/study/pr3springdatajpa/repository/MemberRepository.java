979f3077b09bf3508096a0668460f8d51dc60f2f
yeoro <dufgh1009@naver.com>
2021-12-24 11:23:27 +0900

    [ADD] namedquery

--- pr3-spring-data-jpa/src/main/java/study/pr3springdatajpa/repository/MemberRepository.java
+++ pr3-spring-data-jpa/src/main/java/study/pr3springdatajpa/repository/MemberRepository.java
@@ -3,10 +3,15 @@ package study.pr3springdatajpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import study.pr3springdatajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+	
+	@Query(name = "Member.findByUsername")
+	List<Member> findByUsername(@Param("username") String username);
 }
