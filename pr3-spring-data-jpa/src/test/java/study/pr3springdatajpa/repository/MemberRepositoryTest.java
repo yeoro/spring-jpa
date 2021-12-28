025969c479041660526314f68d8793494db48537
yeoro <dufgh1009@naver.com>
2021-12-28 14:19:36 +0900

    [ADD] JPA Hint & Lock

--- pr3-spring-data-jpa/src/test/java/study/pr3springdatajpa/repository/MemberRepositoryTest.java
+++ pr3-spring-data-jpa/src/test/java/study/pr3springdatajpa/repository/MemberRepositoryTest.java
@@ -229,4 +229,32 @@ class MemberRepositoryTest {
 		
 		List<Member> members = memberRepository.findAll();
 	}
+
+	@Test
+	public void queryHint() {
+		//given
+		Member member = new Member("member1", 10);
+		memberRepository.save(member);
+		em.flush();
+		em.clear();
+
+		//when
+		Member findMember = memberRepository.findReadOnlyByUsername("member1");
+		findMember.setUsername("member2");
+		
+		em.flush();
+	}
+
+	@Test
+	public void lock() {
+		//given
+		Member member = new Member("member1", 10);
+		memberRepository.save(member);
+		em.flush();
+		em.clear();
+		
+		//when
+		List<Member> result = memberRepository.findLockByUsername("member1");
+	}
+
 }
